5727f71112a76493a96e2c33ba2ef465a697a29a
SICOE <sergiu.sicoe@stud.ubbcluj.ro>
2023-11-01 17:42:26 +0200

    uml + Scanner Documentation

--- My_language/Main.py
+++ My_language/Main.py
@@ -1,23 +1,7 @@
 from My_language.Scanner import Scanner
-from My_language.SymTable import SymTable
-
-hash = SymTable()
-
-hash.add("abc")
-hash.add("acb")
-hash.add("cab")
-# print(hash.find("abc"))
-# print(hash.find("acb"))
-# print(hash.find("cab"))
-#
-# print(hash.get_all())
-
-
-# print(divide_program("int a, b, c; \n a == 5; \n read(a);\n read(b);\n read(c);\n if (max(a, b) >= max(b, c)) {\n     write(max(a, b));\n } else {\n    write(c);\n}"))
 
 scanner = Scanner("D:\Semester V\FLCD\FLCD_project_compiler\Lab_1a\p1")
 
-a = 5
 
 
 
